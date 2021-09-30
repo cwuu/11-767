84a2f6bad5761a66f3e420a1f8007545339fe4af
Cheng-hsin Emily WUU <cwuu@andrew.cmu.edu>
2021-09-30 13:01:02 -0400

    complete param count & latency cal

--- labs/lab3.py
+++ labs/lab3.py
@@ -3,29 +3,16 @@ import torchvision.models as models
 import torch 
 from timeit import default_timer as timer
 from PIL import Image
-from torchvision import transforms
+from torchvision import transforms, datasets
 from torch import nn
 
 
-def load_testloader(datadir, input_size=224, bath_size=4):
-    test_transforms = transforms.Compose([transforms.Resize(input_size), transforms.ToTensor(),])
-    test_data = datasets.ImageFolder(datadir, transform=test_transforms)
-    num_train = len(train_data)
-    indices = list(range(num_train))
-    split = int(np.floor(valid_size * num_train))
-    np.random.shuffle(indices)
-    from torch.utils.data.sampler import SubsetRandomSampler
-    test_idx = indices[:split]
-    test_sampler = SubsetRandomSampler(test_idx)
-    testloader = torch.utils.data.DataLoader(test_data, sampler=test_sampler, batch_size=batch_size)
-    return testloader
-
-def run_benchmark(args):
+def run_benchmark(lab2_args):
     unet = torch.hub.load('mateuszbuda/brain-segmentation-pytorch', 'unet',
-    in_channels=3, out_channels=1, init_features=32, pretrained=args['pretrained'])
-    squeezenet = models.squeezenet1_0(pretrained=args['pretrained'])
-    mobilenet_v3_small = models.mobilenet_v3_small(pretrained=args['pretrained'])
-    resnet18 = models.resnet18(pretrained=args['pretrained'])
+    in_channels=3, out_channels=1, init_features=32, pretrained=lab2_args['pretrained'])
+    squeezenet = models.squeezenet1_0(pretrained=lab2_args['pretrained'])
+    mobilenet_v3_small = models.mobilenet_v3_small(pretrained=lab2_args['pretrained'])
+    resnet18 = models.resnet18(pretrained=lab2_args['pretrained'])
     model_zoo = {}
     model_zoo["unet"] = unet 
     model_zoo["squeezenet"] = squeezenet
@@ -33,12 +20,16 @@ def run_benchmark(args):
     model_zoo["resnet18"] = resnet18
     #device = torch.device("cuda" if torch.cuda.is_available()  else "cpu")
     device = "cpu"
-    testloader = load_testloader(args['data_dir'], args['input_size']=224, args['batch_size'])
+
+    test_transforms = transforms.Compose([transforms.Resize(lab2_args['input_size']), transforms.ToTensor(),])
+    test_data = datasets.ImageFolder(lab2_args['data_dir'], transform=test_transforms)
+    testloader = torch.utils.data.DataLoader(test_data, batch_size=lab2_args['batch_size'])
+
     for k, v in model_zoo.items():
         print(" ========= Evaluating: {} =========".format(k))
-        model = v.copy()
+        model = v
         # parameter count
-        num_params = sum([np.prod(p.size()) for p in v.parameters()])
+        num_params = sum([np.prod(p.size()) for p in model.parameters()])
         print(" 1. Param Count: {} ".format(num_params))
         # latency (inference) Time in seconds
         start = timer()
@@ -46,27 +37,33 @@ def run_benchmark(args):
         model.eval()
         criterion = nn.NLLLoss()
         with torch.no_grad():
-            for inputs, labels, in testloader:
+            for inputs, labels in testloader:
                 inputs, labels = inputs.to(device), labels.to(device)
-                    logps = model.forward(inputs)
-                    batch_loss = criterion(logps, labels)
-                    #test_loss += batch_loss.item()
-                    #ps = torch.exp(logps)
-                    #top_p, top_class = ps.topk(1, dim=1)
-                    #equals = top_class == labels.view(*top_class.shape)
-                    #accuracy += torch.mean(equals.type(torch.FloatTensor)).item()
+                output = model.forward(inputs)
+                break
+                #test_loss += batch_loss.item()
+                #ps = torch.exp(logps)
+                #top_p, top_class = ps.topk(1, dim=1)
+                #equals = top_class == labels.view(*top_class.shape)
+                #accuracy += torch.mean(equals.type(torch.FloatTensor)).item()
         # inference based on args setting
         end = timer()
-        print(" 2. Latency(input_size {}, batch_size {}): {} ".format(args['input_size'], args['batch_size'], (end - start)))
+        print(" 2. Latency(input_size {}, batch_size {}): {} ".format(lab2_args['input_size'], lab2_args['batch_size'], (end - start)))
         # energy use
     print("====== Completed All Model Evaluation ======")
 
-args = {
-    "input_size":60, #256, 512, 1024
+lab2_args = {
+    "input_size":224, #256, 512, 1024
     "batch_size":4,
     "pretrained":True,
-    "data_dir": "./"
+    "data_dir": "/home/nvidia/11767/lab2/tiny-imagenet-200/test/"
 
 }
 if __name__ == "__main__":
-    run_benchmark(args)
+    input_sizes = [224, 448, 896]
+    batch_sizes = [1, 2, 4, 8, 16, 32]
+    for i in input_sizes:
+        for b in batch_sizes:
+            lab2_args["input_size"] = i
+            lab2_args["batch_size"] = b
+            run_benchmark(lab2_args)
